2b47409459cafe2b72cf2a9e2239a32bbc28454c
MohanRaj <mraj86698@gmail.com>
2022-08-26 20:39:08 +0530

    Welcome to Hotel Reservation System Program

---
+++ HotelReservationSystem/src/main/java/com/java/maven/HotelReservationSystem/HotelReservation.java
@@ -0,0 +1,9 @@
+package com.java.maven.HotelReservationSystem;
+
+public class HotelReservation
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Hotel Reservation Program" );
+    }
+}
---
+++ HotelReservationSystem/src/test/java/com/java/maven/HotelReservationSystem/HotelReservationTest.java
@@ -0,0 +1,16 @@
+package com.java.maven.HotelReservationSystem;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+public class HotelReservationTest
+{
+
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
